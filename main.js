43ce94c5830b00253d1df172ce897708212ef528
TetianaZakharova <44653360+TetianaZakharova@users.noreply.github.com>
2019-11-21 23:34:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function startRotate(event){
   const halfheight = cardImg.offsetHeight / 2;
   const halfwidth = cardImg.offsetWidth / 2;
 
-  cardImg.style.transform = 'rotateX('+(event.offsetY - halfheight) / 5 +'deg) rotateY('+-(event.offsetX - halfwidth) / 5 +'deg)'
+  cardImg.style.transform = 'rotateX(' + (event.offsetY - halfheight) / 5 +'deg) rotateY('+-(event.offsetX - halfwidth) / 5 +'deg)'
 }
 
 function stopRotate(event){
@@ -18,7 +18,7 @@ function stopRotate(event){
   cardImg.style.transform = 'rotate(0)';
 }
 
- window.addEventListener('scroll', parallax);
+  window.addEventListener('scroll', parallax);
 
   function parallax()  {
     const elem = window.pageYOffset;
@@ -32,3 +32,20 @@ function stopRotate(event){
       }); 
     }
   }
+
+  window.onscroll = (function() {
+  
+    if (window.pageYOffset > 75){
+       document.querySelector('.fixed').classList.add("show");
+       document.querySelector('.order-button').classList.add("fade-button");
+       document.querySelector('.header-logoRight').classList.add("fade-logo");
+       document.querySelector('.header-logoLeft').classList.add("fade-logo");
+       document.querySelector('.header-nav-img').classList.add("img-invert");  
+    } else {
+      document.querySelector('.fixed').classList.remove("show");
+      document.querySelector('.order-button').classList.remove("fade-button");
+      document.querySelector('.header-logoRight').classList.remove("fade-logo");
+      document.querySelector('.header-logoLeft').classList.remove("fade-logo");
+      document.querySelector('.header-nav-img').classList.remove("img-invert");
+     };   	
+  });
